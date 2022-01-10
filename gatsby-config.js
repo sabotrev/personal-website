f84a58e44bd916b560518b7404a854adb3010595
Trevor Sabo <sabotrev@gmail.com>
2022-01-10 13:34:42 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Trevor Sabo Personal Website",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+  ],
+};
