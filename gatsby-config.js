c0bffebead726672aa1ec490f80ed41d927fd61e
Trevor Sabo <sabotrev@gmail.com>
2022-01-20 17:27:33 -0800

    Update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,17 @@
 module.exports = {
     siteMetadata: {
-        siteUrl: 'https://www.yourdomain.tld',
-        title: 'Trevor Sabo Personal Website',
+        siteUrl: 'https://www.trevorsabo.com',
+        title: 'Trevor Sabo',
+        description: `Trevor Sabo's Personal Website`,
     },
     plugins: [
+        `gatsby-plugin-styled-components`,
+        'gatsby-plugin-netlify',
         {
             resolve: 'gatsby-plugin-manifest',
             options: {
                 icon: 'src/images/icon.png',
             },
         },
-        `gatsby-plugin-styled-components`,
     ],
 };
